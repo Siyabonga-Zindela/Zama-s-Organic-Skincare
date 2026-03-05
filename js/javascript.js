4433d7cfe7a019ffbd8dc56fe57aaadb88dacd26
Siyabonga-Zindela <siyabongazindela@outlook.com>
2026-03-05 17:11:01 +0200

    Added all the product description pages

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,12 @@
 let btnMenu = document.getElementById("btn-menu");
 let sideBar = document.querySelector(".sidebar-list");
 let closeSideBar = document.getElementById("btn-close-sidebar");
+let body = document.getElementsByTagName("body");
 
 btnMenu.addEventListener("click", () => {
     if (sideBar.style.right === "-225px") {
         sideBar.style.right = "0px";
+        body.style.backGroundColor = "grey";
     } else {
         sideBar.style.right = "-225px";
     }
